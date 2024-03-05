813e467b6f62dedd97ddadd88c17adcecd343fb0
Kacper Sokołowski <kontakt@kaapa.pl>
2024-03-05 20:56:13 +0100

    Implementacja testów w Jest

--- tests/division.spec.js
+++ tests/division.spec.js
@@ -1,40 +1,30 @@
 import { division } from "../src/division.js";
 
-function test1() {
+test("Test dzielenia", () => {
     const a = 42;
     const b = 6;
     const expectedResult = 7;
 
     const result = division(a, b);
-    if (result === expectedResult) {
-        console.log("Dzielenie działa poprawnie");
-    }
-}
 
-test1();
+    expect(result).toEqual(expectedResult);
+});
 
-function test2() {
+test("Test dzielenia przez zero", () => {
     const a = 25;
     const b = 0;
 
     const expectedResult = Infinity;
 
     const result = division(a, b);
-    if (result === expectedResult) {
-        console.log("Dzielenie przez zero działa poprawnie!");
-    }
-}
 
-function test3() {
+    expect(result).toEqual(expectedResult);
+});
+
+test("Test dzielenia napisów przez siebie", () => {
     const a = "a";
     const b = "c";
 
     const result = division(a, b);
-    if (isNaN(result)) {
-        console.log("Dzielenie napisów przez siebie zwraca NaN");
-    }
-}
-
-test1();
-test2();
-test3();
+    expect(result).toEqual(NaN);
+});
--- tests/profits.spec.js
+++ tests/profits.spec.js
@@ -1,16 +1,10 @@
 import { yearlyProfitCalculator } from "../src/profits.js";
 
-function test1() {
+test("Test liczenia zysków z lokat", () => {
     const amount = 1000;
     const interest = 10;
     const expectedResult = 81;
 
     const result = yearlyProfitCalculator(amount, interest);
-    console.log(result);
-
-    if (result === expectedResult) {
-        console.log("Zysk z lokaty liczy się poprawnie");
-    }
-}
-
-test1();
+    expect(result).toEqual(expectedResult);
+});
